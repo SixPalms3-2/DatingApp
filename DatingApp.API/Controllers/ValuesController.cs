0f77ffe8f3d19edfaf29501b74a6223b634d6198
curtis <curtispienaar11@gmail.com>
2019-10-04 23:34:30 +0200

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<Value> _logger;
+
+        public ValuesController(DataContext context, ILogger<Value> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            // return HTTP response
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+
+            // return HTTP response
+            return Ok(value);
+        }
+
+    }
+}
+
---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="options"></param>
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+
+    }//end class
+}
